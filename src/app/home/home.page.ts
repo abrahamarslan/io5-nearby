1ee43ec6dfbd06013a90166b6596e676c3f37fff
abrahamarslan <arslan@abrahamarslan.com>
2020-05-25 11:51:57 -0700

    reverseGeocode

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {NavController, Platform} from "@ionic/angular";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {GeocoderService} from "../geocoder.service";
 
 @Component({
   selector: 'app-home',
@@ -6,7 +9,59 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  //Public variables
+  public form: FormGroup;
+  public geoForm: FormGroup;
+  public geocoded: boolean;                         //Geocoded or not?
+  public results: string;                           //Geocoding results
+  public filter: string = 'Search by co-ordinates';
+  public displayForwardForm: boolean = true;
+  public displayReverseForm: boolean = false;
 
-  constructor() {}
+  constructor(public navCtrl: NavController, public geocoder: GeocoderService, private _fb: FormBuilder, private _platform: Platform) {
+      this.form = _fb.group({
+        'keyword': ['', Validators.required]
+      });
+
+      this.geoForm = _fb.group({
+        'latitude': ['', Validators.required],
+        'longitude': ['', Validators.required],
+      });
+
+    /**
+     * Display form options
+     */
+      filterForm() {
+        if(this.displayForwardForm) {
+          this.filter = 'Search by keyword';
+          this.displayReverseForm = false;
+          this.displayForwardForm = true;
+        } else {
+          this.filter = 'Search by co-ordinates';
+          this.displayReverseForm = true;
+          this.displayForwardForm = false;
+        }
+    }
+    /**
+     * @public
+     * @return {none}
+     */
+    reverseGeocode(val) {
+      this._platform.ready().then((data: any) => {
+          let latitude: any = parseFloat(this.geoForm.controls["latitude"].value),
+              longitude: any = parseFloat(this.geoForm.controls["longitude"].value);
+          this.geocoder.reverseGeocode(latitude, longitude)
+              .then((data: any) => {
+                this.geocoded = true;
+                this.results = data;
+              });
+      })
+      .catch((error: any) => {
+        //Todo: Display and log error information
+        this.geocoded = false;
+        this.results = error.message;
+      });
+    }
+  }
 
 }
